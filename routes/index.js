d14fd8b928c1294fb900e029e6ba1356bb5561e5
Al-27 <125827852+Al-27@users.noreply.github.com>
2024-08-07 05:25:26 +0000

    file route, file controllers

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import appController from '../controllers/AppController';
 import userCtrls from '../controllers/UsersController';
 import authCtrls from '../controllers/AuthController';
+import filesCtrls from '../controllers/FilesController';
 
 const routes = express.Router();
 
@@ -16,4 +17,5 @@ routes.get('/disconnect', authCtrls.disconnect);
 routes.get('/users/me', userCtrls.curUsr);
 routes.post('/users', userCtrls.newUser);
 
+routes.post('/files', filesCtrls.upload);
 export default routes;
