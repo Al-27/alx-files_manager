067b737fbb1f4877dc6d82d9edf3e63b85ef9fc9
Al-27 <125827852+Al-27@users.noreply.github.com>
2024-10-03 02:49:45 +0000

    use writefilesync

--- utils/misc.js
+++ utils/misc.js
@@ -1,5 +1,6 @@
 import {
-  mkdirSync, writeFile, createReadStream, statSync, constants, accessSync,
+  mkdirSync, createReadStream, statSync, constants, accessSync,
+  writeFileSync,
 } from 'fs';
 import path from 'path';
 import { env } from 'process';
@@ -7,14 +8,16 @@ import { v4 } from 'uuid';
 import { contentType } from 'mime-types';
 import redisClient from './redis';
 
-function CreateFile(file, parent = '') {
+//@fl: could be a folder or a file
+function CreateFile(fl, parent = '') {
   let localPath = env.FOLDER_PATH ? env.FOLDER_PATH : '/tmp/files_manager';
+  console.log(fl, parent);
   localPath = path.join(localPath, parent);
   const folderPath = path.join(localPath);
   mkdirSync(folderPath, { recursive: true });
   const uuid = v4();
-  const data = Buffer.from(file, 'base64');
-  writeFile(path.join(folderPath, uuid), data, (er) => { if (er) console.log(er); });
+  const data = Buffer.from(fl, 'base64');
+  writeFileSync(path.join(folderPath, uuid), data);
 
   return path.join(localPath, uuid);
 }
